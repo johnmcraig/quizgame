427175747a78a44d5799e7c1dacb09a1f634e309
johnmcraig <jmcraig32884@gmail.com>
2018-01-07 23:02:03 -0500

    Added DB conext and first view model

---
+++ ViewModel/QuizViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace QuizTaker.ViewModel
+{
+    [JsonObject(MemberSerialization.OptOut)]
+    public class QuizViewModel
+    {
+        public QuizViewModel()
+        {
+
+        }
+
+        public int Id{ get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Text { get; set; }
+
+    }
+}
