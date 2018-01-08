1ccf60f7b39f8072aadd15741f0de2c2e44bd372
johnmcraig <jmcraig32884@gmail.com>
2018-01-07 22:40:58 -0500

    Initial Commit

---
+++ Data/Models/ApplicationUser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizTaker.Data.Models
+{
+    public class ApplicationUser
+    {
+        //region Constructor
+        public ApplicationUser()
+        {
+
+        }
+        //endregion
+
+        //region Properties
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(128)]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+        public string DisplayName { get; set; }
+        public string Notes { get; set; }
+
+        [Required]
+        public int Type { get; set; }
+        [Required]
+        public int Flags { get; set; }
+        [Required]
+        public DateTime LastModifiedDate { get; set; }
+    }
+}
---
+++ Data/Models/Quiz.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QuizTaker.Data.Models
+{
+    public class Quiz
+    {
+        public Quiz()
+        {
+
+        }
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+    }
+}
