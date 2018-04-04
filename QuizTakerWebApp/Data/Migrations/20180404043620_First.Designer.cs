5029d41a44863ec130480efd9f6d10dbfd0d281a
johnmcraig <jmcraig32884@gmail.com>
2018-04-04 00:38:06 -0400

    Made first migration of database and updated

--- QuizTakerWebApp/Data/Migrations/20180404043620_First.Designer.cs
+++ QuizTakerWebApp/Data/Migrations/20180404043620_First.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace QuizTaker.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180404043620_First")]
+    partial class First
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
