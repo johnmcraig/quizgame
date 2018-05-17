b6d5a95462a98301da98cb2604f6164b80a8e22c
johnmcraig <jmcraig32884@gmail.com>
2018-05-16 22:55:07 -0400

    Updated layout and main javascript file

--- QuizTakerWebApp/wwwroot/js/quizgame.js
+++ QuizTakerWebApp/wwwroot/js/quizgame.js
@@ -1,6 +1,30 @@
-﻿$(document).ready(function () {
+﻿// Test JSON by returning objects into console output
+$(document).ready(function () {
     $.getJSON("/api/QuizGame")
     function Data(data) {
         console.log(data)
     } 
 });
+// The actual path as a uri then call AJAX then bind the data to a list element
+const uri = "/api/QuizGame/";
+let quizzes = null;
+
+$(document).ready(function () {
+    $.getJSON(uri)
+        .done(function (data) {
+            $.each(data, function (key, quiz) {
+                $('<li>', { text: formatQuiz(quiz) }).appendTo($('#quiz-title'));
+            });
+        });
+});
+// find quiz by id :: Url = api/QuizGame/id
+function find() {
+    var id = $('#quizId').val();
+    $.getJSON(apiUrl + '/' + id)
+        .done(function (data) {
+            $('#quiz-title').text(formatItem(data));
+        })
+        .fail(function (jqXHR, testStatus, err) {
+            $('#quiz-title').text('Error' + err);
+        });
+}
