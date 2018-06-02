0d0c1b45d3051cc4082d7ef93896a8e3c25baaf6
johnmcraig <jmcraig32884@gmail.com>
2018-06-02 18:17:17 -0400

    Added Vue.js library and created app.js, removed deprecated jQuery

--- QuizTakerWebApp/wwwroot/js/app.js
+++ QuizTakerWebApp/wwwroot/js/app.js
@@ -1,28 +1,53 @@
-﻿$(document).ready(function () {
-
-    var ulQuizzes = $('#ulQuizzes');
-
-    $('#get-btn').click(function () {
-        $.ajax({
-            type: 'GET',
-            url: 'api/QuizGame',
-            dataType: 'json',
-            success: function (data) {
-
-                ulQuizzes.empty();
-                $.each(data, function (index, val) {
-                    var quizId = val.quizId;
-                    var quizTitle = val.title;
-                    var quizQuestion = val.questions;
-                    var quizAnswer = val.answers;
-                    ulQuizzes.append('<li>' + quizId + ' ' + quizTitle + ' ' + quizQuestion + ' ' + quizAnswer + '</li>');
-                });
-            }
-        });
-    });
-
-    $('#clear-btn').click(function () {
-        ulQuizzes.empty();
-    });
-
+﻿var app = new Vue({
+    el: "#index-page",
+    data: {
+        appName: "Quiz Game",
+        error: "",
+        quiz: [],
+        questions: [],
+        answers: []
+    },
+    methods: {
+        nextQuiz: function (next) {
+            this.quiz++;
+        },
+        prevQuiz: function (prev) {
+            this.quiz--;
+        }  
+    },
+    mounted: function () {
+        //CALL API
+        axios.get("/api/QuizGame")
+            .then(function (response) {
+                this.quiz = response.data[0]
+                this.questions = response.data[0].questions
+                this.answers = response.data[0].questions[0].answers
+            }.bind(this))
+            .catch(function () {
+                this.error = "Could not load Quizzes" 
+            }.bind(this));
+    }
 });
+
+/*
+ *  Start 
+ *  var quizzes = [];
+ *  var currentQuizId = 0;
+ *  var currentQuestionId = 0;
+ *  ?? var currentAsnwerId = 0; ??
+ *  []
+ *  Quiz
+ *      Questions []
+ *          Question
+ *              Content
+ *              Answer []
+ *                  Content
+ *                  isCorrect
+ * Select Quiz:
+ * Next Question Button
+ *  -> Get currentQuestion from currentQuiz
+ *      innerHTML = Question.text
+ *          for ( var answer in questions )
+ * 
+ * 
+*/
--- QuizTakerWebApp/wwwroot/js/site.js
+++ QuizTakerWebApp/wwwroot/js/site.js
@@ -1,106 +1,152 @@
-﻿/* JSON Results from POSTMan
-quizId,
-    title,
-    author,
-    publishDate,
-  questions:
-        questionId + quizId,
-        content
-  answers:
-         answerId,
-         content,
-         isCorrect boolean(true, false)
-*/
+﻿$(document).ready(function () {
 
-var answersContainer = document.getElementById("answers-container"),
-    questionContent = document.getElementById("question-content"),
-    answerResultContainer = document.getElementById("answer-result-container"),
-    nextBtn = document.getElementById("next-button"),
-    previousBtn = document.getElementById("previous-button");
+    var ulQuizzes = $('#ulQuizzes');
+    var ulQuestions = $('#ulQuestions');
+    let uri = 'api/QuizGame';
+
+    $.getJSON(uri, function (data) {
+        $.each(data.quizId, function (i, title) {
+            console.log(title);
+        });
+    });
+
+    $('#get-btn').click(function () {
+        $.ajax({
+            type: 'GET',
+            url: 'api/QuizGame',
+            dataType: 'json',
+            success: function (data) {
+
+                ulQuizzes.empty();
+
+                $.each(data, function (index, val) {
+                    var quizId = val.quizId;
+                    var quizTitle = val.title;
+                    var authName = val.author;
+                    var pubDate = val.publishDate;
+
+                    var questions = val.questions;
+
+                    
+                    var answerId = val.answerId;
+                    var quizQuestion = val.questions;
+                    var quizAnswer = val.answers;
+
+                    ulQuizzes.append('<li>' + quizId + ' ' + quizTitle + ' ' + authName + ' ' + pubDate + '</li>');
+
+                    $.each(questions, function (index, val) {
+                        ulQuestions.append('<li>' + questions + ' ' + '</li>');
+                    });
 
 
-var quizGame = {
-    questions: {},
-    current: 0
-};
+                });
+            }
+        });
+    });
 
-// Get questions from API on page load
-$(document).ready(function () {
+    $('#clear-btn').click(function () {
+        ulQuizzes.empty();
+        questionLi.empty();
+    });
 
-    var quizId = document.getElementById("quizGameId").innerText;
+    $('#next-btn').click(function () {
 
-    var path = "/api/QuizGame/" + quizId;
+    });
 
-    $.getJSON(path, function (data) {
-        quizGame.questions = data;
-        renderQuestion();
+    $('#previous-btn').click(function () {
+
     });
 
 });
 
-// Render out question and answers
-function renderQuestion() {
+// Get questions from API on page load 
+$(document).ready(function () {
+    var quizId = document.getElementById("quizId").innerText;
+    var path = "/api/QuizGame/" + quizId;
 
-    questionContent.innerText = quizGame.questions[quizGame.current].questions;
-
-    var count = 0;
-
-    for (var answer in quizGame.questions.[quizGame.current].answers) {
-
-
-        var answerLi = document.createElement("li"),
-            answerPosition = document.createElement("span");
-
-
-        answerPosition.innerText = count++;
-        answerPosition.classList.add("hidden");
-        answerLi.classList.add("list-group-item");
-        answerLi.classList.add("answer-item");
-        answerLi.innerText = quizGame.questions[quizGame.current].answers[answer].content;
-
-        answerLi.appendChild(answerPosition);
-        answersContainer.appendChild(answerLi);
-    }
-}
-
-// Clear elements on click
-function clearElements() {
-    answersContainer.innerHTML = "";
-    questionContent.innerText = "";
-    answerResultContainer.innerText = "";
-}
-
-// On answer click events
-$(document).on('click', ".answer-item", function (event) {
-
-    var result = quizGame.questions[quizGame.current].answers[event.currentTarget.getElementsByTagName("span")[0].innerText].isCorrect;
-
-    if (result) {
-        answerResultContainer.classList.remove("wrong");
-        answerResultContainer.classList.add("correct");
-    } else {
-        answerResultContainer.classList.remove("correct");
-        answerResultContainer.classList.add("wrong");
-    }
-
-    answerResultContainer.innerText = result.toString().toUpperCase();
-
+    $.getJSON(path, function (data) {
+        game.questions = data;
+        renderQuestion();
+    });
 });
 
-// Next button events 
-nextBtn.addEventListener("click", function () {
-    if (quizGame.current < quizGame.questions.length - 1) {
-        quizGame.current++;
-    }
-    clearElements();
-    renderQuestion();
-});
+/*
+ *
+ * Original jQuery that is no longer used but may be usefull later
+ * 
+ */
 
-// Previous button events
-previousBtn.addEventListener("click", function () {
-    if (quizGame.current > 0) {
-        quizGame.current--;
-    }
-    clearElements();
-    renderQuestion();
-});
+//// Game data object 
+//var game = {
+//    questions: {},
+//    current: 0,
+//}
+
+//// Variables for elements
+//var answersHolder = document.getElementById("answers-container"),
+//    questionContent = document.getElementById("question-content"),
+//    nextBtn = document.getElementById("next-btn"),
+//    previousBtn = document.getElementById("previous-btn"),
+//    answerResultHolder = document.getElementById("answer-result-container");
+
+
+//// Render out question and answers
+//function renderQuestion() {
+//    questionContent.innerText = game.questions[game.current].questions;
+//    var count = 0;
+//    for (var answer in game.questions[game.current].answers) {
+
+//        var answerLi = document.createElement("li"),
+//            answerPosition = document.createElement("span");
+
+//        answerPosition.innerText = count++;
+//        answerPosition.classList.add("hidden");
+//        answerLi.classList.add("list-group-item");
+//        answerLi.classList.add("answer-item");
+//        answerLi.innerText = game.questions[game.current].answers[answer].content;
+
+//        answerLi.appendChild(answerPosition);
+//        answersHolder.appendChild(answerLi);
+//    };
+//};
+
+//// Next button events 
+//nextBtn.addEventListener("click", function () {
+//    if (game.current < game.questions.length - 1) {
+//        game.current++;
+//    }
+//    clearElements();
+//    renderQuestion();
+//})
+
+//// Previous button events
+//previousBtn.addEventListener("click", function () {
+//    if (game.current > 0) {
+//        game.current--;
+//    }
+//    clearElements();
+//    renderQuestion();
+//})
+
+//// Clear elements function
+//function clearElements() {
+//    answersHolder.innerHTML = "";
+//    questionContent.innerText = "";
+//    answerResultHolder.innerText = "";
+//}
+
+//// On answer click events
+//$(document).on('click', ".answer-item", function (event) {
+//    var result = game.questions[game.current].Answers[event.currentTarget.getElementsByTagName("span")[0].innerText].isCorrect;
+
+//    if (result) {
+//        answerResultHolder.classList.remove("wrong");
+//        answerResultHolder.classList.add("correct");
+//    } else {
+//        answerResultHolder.classList.remove("correct");
+//        answerResultHolder.classList.add("wrong");
+//    }
+
+//    answerResultHolder.innerText = result.toString().toUpperCase();
+
+//});
