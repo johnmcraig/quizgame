73adf7977bec9c617485c3fba78dcfe9255a54f7
johnmcraig <jmcraig32884@gmail.com>
2018-06-03 21:44:34 -0400

    Added Vue event binding for clicking answers

--- QuizTakerWebApp/wwwroot/js/app.js
+++ QuizTakerWebApp/wwwroot/js/app.js
@@ -3,22 +3,38 @@
     data: {
         quiz: [],
         question: 0,
+        clicked: false,
+        answered: false,
+        correct: null,
+        count: 0
     },
     methods: {
         nextQuiz: function () {
             this.question++
+            this.answered = false
+            this.correct = null
         },
         prevQuiz: function () {
             this.question--
-        }  
+            this.answered = false
+            this.correct = null
+        },
+        answerEvent(isCorrect) {
+            this.answered = true
+            if (isCorrect === true) {
+                this.correct = true
+            } else {
+                this.correct = false
+            }
+        }
     },
     mounted: function () {
         axios.get("/api/QuizGame")
             .then(function (response) {
                 this.quiz = response.data
             }.bind(this))
-            .catch(function () {
-                this.error = "Could not load Quizzes" 
+            .catch(function (error) {
+                this.error = "Could not load Quizzes"
             }.bind(this));
     }
 });
